2f0e24b20f61e34412b5db36b2e1f0de84597700
duyidong <duyidong@umich.edu>
2020-09-06 00:02:07 -0400

    EXPERIMENT: Add a faster way to traverse map

--- experiment/data_collection.py
+++ experiment/data_collection.py
@@ -15,7 +15,7 @@ parser.add_argument("--scene_type", type=int, default=1,  help="Choose scene typ
 parser.add_argument("--scene_num", type=int, default=0,  help="Choose scene num for simulation, from 1 - 30")
 parser.add_argument("--grid_size", type=float, default=0.25,  help="Grid size of AI2THOR simulation")
 parser.add_argument("--rotation_step", type=float, default=10,  help="Rotation step of AI2THOR simulation")
-parser.add_argument("--sleep_time", type=float, default=0.05,  help="Sleep time between two actions")
+parser.add_argument("--sleep_time", type=float, default=0.005,  help="Sleep time between two actions")
 parser.add_argument("--save_directory", type=str, default='./data',  help="Data saving directory")
 parser.add_argument("--overwrite_data", type=bool, default=False, help="overwrite the existing data or not")
 parser.add_argument("--log_level", type=int, default=5,  help="Level of showing log 1-5 where 5 is most detailed")
@@ -86,11 +86,48 @@ class Dumb_Navigetion():
 					self._map[point_added_index].append(self._point_num - 1)
 		return
 
-	def Dumb_traverse_map(self):
+	def Fast_traverse_map(self, goal_points_num):
+		self._map_searched = [True] * len(self._coordinate_dict)
+		goal_points = []
+		polar_coordinate = []
+		current_position = list(self._starting_point.values())
+		for point in self._point_list:
+			polar_coordinate.append([np.arctan2(point[0] - current_position[0],	point[2] - current_position[2]) / np.pi * 180,
+				np.sqrt((point[0] - current_position[0])**2 + (point[2] - current_position[2])**2)])
+		# print(polar_coordinate)
+		polar_coordinate_copy = copy.deepcopy(polar_coordinate)
+		polar_coordinate_copy = sorted(polar_coordinate_copy, key=lambda x: x[0])
+		# print(polar_coordinate_copy)
+		max_dis = [-1] * goal_points_num
+		default_polar = [-181, -1]
+		goal_points_polar_coordinate = [default_polar] * goal_points_num
+		degree_division = 360.0 / goal_points_num
+		for polar_point in polar_coordinate_copy:
+			degree_division_num = int((polar_point[0] + 180) / degree_division - 0.0001)
+			# print(degree_division_num)
+			if polar_point[1] > max_dis[degree_division_num]:
+				max_dis[degree_division_num] = polar_point[1]
+				goal_points_polar_coordinate[degree_division_num] = polar_point
+		print(goal_points_polar_coordinate)
+		for goal_point_polar in goal_points_polar_coordinate:
+			if not goal_point_polar == default_polar:
+				goal_points.append(polar_coordinate.index(goal_point_polar))
+		print(goal_points)
+		for goal_point_index in goal_points:
+			self.Dumb_navigate(goal_index=goal_point_index)
+			self.Dumb_navigate(goal_position=self._starting_point)
+
+
+	def Dumb_traverse_map(self, complete=False):
 		searching_queue = []
 		# print(self._point_list)
 		# print(self._Agent_action.Get_agent_position())
+		searched_map = list(filter(lambda x: x == True, self._map_searched))
+		searched_num_start = len(searched_map)
 		while False in self._map_searched:
+			if not complete:
+				if searched_num_start > int(len(self._map_searched) / 5):
+					break
 			current_position_index = self._point_list.index(list(self._Agent_action.Get_agent_position().values()))
 			if current_position_index in searching_queue:
 				searching_queue.remove(current_position_index)
@@ -98,16 +135,21 @@ class Dumb_Navigetion():
 				if connected_point_index not in searching_queue and not self._map_searched[connected_point_index]:
 					searching_queue.insert(0, connected_point_index)
 			if self._debug:
-				print(searching_queue)
+				print('searching_queue: ', searching_queue)
+				print('self._map connected: ', self._map[searching_queue[0]])
+				# print('self._map_searched: ', )
+				for point_temp in self._map[searching_queue[0]]:
+					print('searched or not: ', point_temp, self._map_searched[point_temp])
 			if len(searching_queue) == 0:
 				break
 			if not self.Dumb_navigate(self._point_list[searching_queue[0]], searching_queue[0]):
 				logging.error('Fail traversing the map')
 				break
+			searched_num_start += 1
 		return
 
 		# Assume goal_position is dict
-	def Dumb_navigate(self, goal_position, goal_index=None):
+	def Dumb_navigate(self, goal_position=None, goal_index=None):
 		graph = Graph()
 		nav_starting_point = self._Agent_action.Get_agent_position()
 		nav_starting_point = list(nav_starting_point.values())
@@ -134,6 +176,8 @@ class Dumb_Navigetion():
 			return False
 
 		connected_point_index = self._map[goal_point_index]
+		if self._debug:
+			print('connected_point_index: ', connected_point_index)
 		nearest_reachable_index = None
 		goal_in_existing_map = False
 		if self._map_searched[goal_point_index]:
@@ -336,7 +380,6 @@ class Agent_action():
 		for _ in range(rotate_steps):
 			if self._debug:
 				time.sleep(self._sleep_time)
-				print('sleep')
 			action = self.Unit_rotate(self._rotation_step * np.sign(rotation_error_corrected))
 			if action:
 				self._Save_RGB_label(self._action_type[action])
@@ -346,7 +389,6 @@ class Agent_action():
 
 		if self._debug:
 				time.sleep(self._sleep_time)
-				print('sleep')
 		action = self.Unit_move()
 		if action:
 			self._Save_RGB_label(self._action_type[action])
@@ -389,24 +431,35 @@ class Agent_action():
 
 
 if __name__ == '__main__':
-	print(args.debug)
 	Dumb_Navigetion = Dumb_Navigetion(args.scene_type, args.scene_num, args.grid_size,
 		args.rotation_step, args.sleep_time, args.save_directory, overwrite_data=args.overwrite_data, debug=args.debug)
-	print(Dumb_Navigetion._debug)
+	# print(Dumb_Navigetion._debug)
+	reach = Dumb_Navigetion._Agent_action.Get_reachable_coordinate()
+	Dumb_Navigetion.Fast_traverse_map(goal_points_num=4)
+
+	exit()
+	x = []
+	y = []
+	for i in range(len(reach)):
+		x.append(reach[i]['z'])
+		y.append(reach[i]['x'])
+	x_array = np.array(x)
+	y_array = np.array(y)
+	plt.scatter(x_array, y_array)
+	plt.show()
+	# print(reach)
 	# position = Dumb_Navigetion.Get_agent_position()
 	# print(position)
-	# exit()
+	exit()
 	Dumb_Navigetion.Open_close_label_text()
 	Dumb_Navigetion.Dumb_traverse_map()
 	position = Dumb_Navigetion.Get_agent_position()
-	print(position)
+	# print(position)
 	ori_position = copy.deepcopy(position)
 	reach = Dumb_Navigetion._Agent_action.Get_reachable_coordinate()
-	print(reach[0:4])
-	position_goal = reach[random.randint(int(len(reach) / 3), len(reach))]
-	Dumb_Navigetion.Dumb_navigate(position_goal)
+	# print(reach[0:4])
 	Dumb_Navigetion.Open_close_label_text()
 	position = Dumb_Navigetion.Get_agent_position()
-	print(position)
-	print(position == position_goal)
+	# print(position)
+	# print(position == position_goal)
 	time.sleep(2)
